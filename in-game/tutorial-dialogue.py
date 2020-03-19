1ca75703b30b9e978cd638fc43eff83c9a8e2ade
Heittor Nunes Biazon <heittor.nunes.b@outlook.com>
2020-03-19 17:40:36 -0300

    Added the tutorial
    Added the tutorial

---
+++ in-game/tutorial-dialogue.py
@@ -0,0 +1,20 @@
+print('Hello, and welcome to Naval Worlds, adventurer!')
+queo = input('Would you like to do a quick tutorial? yes/no ')
+if queo == "yes"
+    print('Ok then, lets go!')
+    print('Naval Worlds is a game where you will battle enemies and earn prizes!')
+    print('Example: This is a monster, attack the monster to earn XP, the more XP you have more power you get!')
+    print('You can battle your friends too, not only bots')
+    print('There are many maps that you can play, choose the best for you to start!')
+    print('You will find much more ways to have fun in Naval Worlds, thats it fot now adventurer! Goodluck!')
+if queo == "no"
+    quetw = input('Are you sure? yes/no ')
+    if quetw == "yes"
+        print('Ok, goodluck adventurer!')
+    if quetw == "no"
+        print('Ok then, lets go!')
+        print('Naval Worlds is a game where you will battle enemies and earn prizes!')
+        print('Example: This is a monster, attack the monster to earn XP, the more XP you have more power you get!')
+        print('You can battle your friends too, not only bots')
+        print('There are many maps that you can play, choose the best for you to start!')
+        print('You will find much more ways to have fun in Naval Worlds, thats it fot now adventurer! Goodluck!')
